0253dda0445a87dee6dddf6f2599df1eed765626
ajkfds <ajkfds2018@gmail.com>
2019-02-08 06:27:27 +0900

    change tab icon graphics

--- codeEditor/Tabs/EditorPage.cs
+++ codeEditor/Tabs/EditorPage.cs
@@ -10,15 +10,37 @@ namespace codeEditor.Tabs
     {
         public EditorPage()
         {
-//            Controls.Add(this.codeEditor);
-            Location = new System.Drawing.Point(4, 28);
+            Controls.Add(this.codeEditor);
+            Location = new System.Drawing.Point(4, 29);
             Name = "editorPage";
             Padding = new System.Windows.Forms.Padding(3);
-            //this.editorPage.Size = new System.Drawing.Size(620, 440);
-            //this.editorPage.TabIndex = 0;
-            //this.editorPage.Text = "code";
-            //this.editorPage.UseVisualStyleBackColor = true;
+            Size = new System.Drawing.Size(620, 439);
+            TabIndex = 0;
+            Text = "code";
+            UseVisualStyleBackColor = true;
+            IconImage = new ajkControls.IconImage(Properties.Resources.text);
+            
+            // 
+            // codeEditor
+            // 
+            codeEditor.BackColor = System.Drawing.Color.White;
+            codeEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            codeEditor.Font = new System.Drawing.Font("MS UI Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            codeEditor.Location = new System.Drawing.Point(3, 3);
+            codeEditor.Name = "codeEditor";
+            codeEditor.Size = new System.Drawing.Size(614, 433);
+            codeEditor.TabIndex = 0;
 
         }
+
+        private CodeEditor.CodeEditor codeEditor = new CodeEditor.CodeEditor();
+        public CodeEditor.CodeEditor CodeEditor
+        {
+            get
+            {
+                return codeEditor;
+            }
+        }
+
     }
 }
