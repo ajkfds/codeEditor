c5d1f1498e01626345d6b1326b384db2e0f6085a
ajkfds <ajkfds2018@gmail.com>
2019-07-12 06:26:37 +0900

    add project property form

---
+++ codeEditor/Tools/ProjectPropertyTab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeEditor.Tools
+{
+    public class ProjectPropertyTab : ajkControls.TabPage
+    {
+        public void PropertyAccept() { }
+        public void PropertyCancel() { }
+    }
+}
