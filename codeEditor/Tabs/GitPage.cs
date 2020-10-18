eeaea9f991d4e6ba81c3d748e67dc12b4b13162b
ajkfds <ajkfds2018@gmail.com>
2020-10-19 05:46:51 +0900

    fix gitpage

--- codeEditor/Tabs/GitPage.cs
+++ codeEditor/Tabs/GitPage.cs
@@ -11,6 +11,22 @@ namespace codeEditor.Tabs
         public GitPage(Data.Project project) : base(new ajkControls.Git.GitPanel(project.RootPath),project.Name)
         {
             IconImage = codeEditor.Global.IconImages.Git;
+
+            gitPanel.BackColor = System.Drawing.Color.White;
+            gitPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            gitPanel.Font = new System.Drawing.Font("MS UI Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            gitPanel.Location = new System.Drawing.Point(3, 3);
+            gitPanel.Name = "codeEditor";
+            gitPanel.Size = new System.Drawing.Size(614, 433);
+            gitPanel.TabIndex = 0;
+        }
+
+        public ajkControls.Git.GitPanel gitPanel
+        {
+            get
+            {
+                return (ajkControls.Git.GitPanel)panel;
+            }
         }
 
     }
